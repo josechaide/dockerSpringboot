1b25c22284a66706b2b3a7a2d00157cc728eccbe
joseba <joseba_echaide_flamarique@carrefour.com>
2020-05-08 21:38:00 +0200

    Initial commit to gitHub

---
+++ src/main/java/com/aubay/docker/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.aubay.docker.demo;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		run(DemoApplication.class, args);
+	}
+
+}
