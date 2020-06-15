c1955e7a1bcbdec3d2da59597c9c2c2cd56297b5
echaidito <josechaide@gmail.com>
2020-06-15 18:30:54 +0200

    edit Jenkinsfile

--- src/main/java/com/aubay/docker/demo/resource/HelloResource.java
+++ src/main/java/com/aubay/docker/demo/resource/HelloResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest/docker/hello")
-
+// hola
 public class HelloResource {
     @GetMapping
     public String hello(){
